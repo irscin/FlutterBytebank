30f0a2d1ccd78e559313c6acd7c72b1c5cbcc283
Italo <irs@cin.ufpe.br>
2020-03-23 14:16:11 -0300

    Pequena refatoração

--- lib/main.dart
+++ lib/main.dart
@@ -6,6 +6,13 @@ class ByteBank extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    return Dashboard();
+  }
+}
+
+class Dashboard extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData.dark(), //Dark theme, obviously
